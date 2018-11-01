4a37392872955b23c0882c4153fa42fe847c0c10
cxv8 <44244680+cxv8@users.noreply.github.com>
2018-11-01 08:44:52 -0700

    guess game, new notes

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -118,3 +118,13 @@ def grade_calc(percentage):
 
 
 print(grade_calc(20))
+
+# Equality Statements
+print(5 > 3)
+print(5 >= 3)
+print(3 == 3)
+
+"""
+a = 3 # A is set to 3
+a == 3 # Is a equal to 3?
+"""
---
+++ notes/Kentzu Xiong - Guess Game.py
@@ -0,0 +1,14 @@
+import random
+print(random.randint(0, 10))
+guess_number = int(input("guess a number between 0 and 10. "))
+if real_number:
+    return ""
+
+"""
+def number(number = input):
+    if number == random.randint:
+        return "Correct number"
+    if number > random.randint:
+        return "guess lower"
+"""
+
