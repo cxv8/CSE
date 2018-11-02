5b57e0f2daa5ea4ff2b2e5f8e1ac655f3e60cff8
cxv8 <44244680+cxv8@users.noreply.github.com>
2018-11-02 13:38:08 -0700

    guess gamee

--- notes/Kentzu Xiong - Guess Game.py
+++ notes/Kentzu Xiong - Guess Game.py
@@ -1,14 +1,95 @@
 import random
-print(random.randint(0, 10))
-guess_number = int(input("guess a number between 0 and 10. "))
-if real_number:
-    return ""
+number = (random.randint(0, 10))
 
-"""
-def number(number = input):
-    if number == random.randint:
-        return "Correct number"
-    if number > random.randint:
-        return "guess lower"
-"""
 
+guess_taken = 0
+
+guess_number = int(input("Guess a number between 0 and 10. "))
+
+
+guess_taken = guess_taken + 1
+if guess_number < number:
+    print("Your guess is to low")
+elif guess_number > number:
+    print("Your guess is to high")
+elif guess_number == number:
+    print("You guessed it!")
+elif guess_number == number:
+    print("You guess it" + guess_taken)
+if guess_taken > 5:
+    print("Sorry my guess was" + guess_taken)
+
+if guess_number == number:
+    print(guess_taken)
+
+
+guess_taken = 0
+
+guess_number = int(input("Guess a number between 0 and 10. "))
+
+
+guess_taken = guess_taken + 1
+if guess_number < number:
+    print("Your guess is to low")
+elif guess_number > number:
+    print("Your guess is to high")
+elif guess_number == number:
+    print("You guessed it!")
+elif guess_number == number:
+    print("You guess in" + guess_taken)
+elif guess_taken > 5:
+    print("My guess was" + guess_taken)
+
+
+guess_taken = 0
+
+guess_number = int(input("Guess a number between 0 and 10. "))
+
+
+guess_taken = guess_taken + 1
+if guess_number < number:
+    print("Your guess is to low")
+elif guess_number > number:
+    print("Your guess is to high")
+elif guess_number == number:
+    print("You guessed it!")
+elif guess_number == number:
+    print("You guess in" + guess_taken)
+elif guess_taken > 5:
+    print("My guess was" + guess_taken)
+
+
+guess_taken = 0
+
+guess_number = int(input("Guess a number between 0 and 10. "))
+
+
+guess_taken = guess_taken + 1
+if guess_number < number:
+    print("Your guess is to low")
+elif guess_number > number:
+    print("Your guess is to high")
+elif guess_number == number:
+    print("You guessed it!")
+elif guess_number == number:
+    print("You guess in" + guess_taken)
+elif guess_taken > 5:
+    print("My guess was" + guess_taken)
+
+
+guess_taken = 0
+
+guess_number = int(input("Guess a number between 0 and 10. "))
+
+
+guess_taken = guess_taken + 1
+if guess_number < number:
+    print("Your guess is to low")
+elif guess_number > number:
+    print("Your guess is to high")
+elif guess_number == number:
+    print("You guessed it!")
+elif guess_number == number:
+    print("You guess in" + guess_taken)
+elif guess_taken > 5:
+    print("My guess was" + guess_taken)
