99fcac8e32223e78b3e0e494bb7cbdc79bb659ec
cxv8 <44244680+cxv8@users.noreply.github.com>
2018-11-06 08:43:24 -0800

    completedd guess

--- notes/Kentzu Xiong - Guess Game.py
+++ notes/Kentzu Xiong - Guess Game.py
@@ -1,6 +1,6 @@
 import random
 number = (random.randint(0, 10))
-guess_taken = 0
+guess_taken = 1
 guess_number = int(input("Guess a number between 0 and 10. "))
 if guess_number < number:
     print("Your guess is to low")
@@ -10,7 +10,7 @@ elif guess_number == number:
     print("You guessed it!. It took %s guesses." % guess_taken)
 if guess_taken == 5:
     print("My guess was %s" % number)
-while guess_taken < 5 and guess_number != number:
+while guess_taken < 4 and guess_number != number:
     guess_number = int(input("Guess a number between 0 and 10. "))
     guess_taken += 1
     if guess_number < number:
@@ -19,5 +19,5 @@ while guess_taken < 5 and guess_number != number:
         print("Your guess is to high")
     elif guess_number == number:
         print("You guessed it!. It took %s guesses." % guess_taken)
-    if guess_taken == 5:
-        print("My guess was %s" % number)
+    if guess_taken == 4:
+        print("My guess was %s." % number)
