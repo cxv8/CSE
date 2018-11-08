9adedc0408affb5fad422bfb20a84bf078470275
cxv8 <44244680+cxv8@users.noreply.github.com>
2018-11-08 08:42:30 -0800

    lucky closer

--- notes/Kentzu Xiong - Lucky 7s.py
+++ notes/Kentzu Xiong - Lucky 7s.py
@@ -1,20 +1,17 @@
 import random
-
-dice1 = (random.randint(1, 6))
-dice2 = (random.randint(1, 6))
-round = int(input("How many rounds do you want to play? "))
 amount_of_money = 15
-amount_of_money -= 1
-if dice1 + dice2 == 7:
-    print("You win! You get $5 back.")
-    amount_of_money += 4
-    print(amount_of_money)
-for round > 0:
+rounds = 0
+print("You have $15 and bet $1. If you win you get $4 back.")
+i = amount_of_money
+while i in range(i > 0):
+    rounds += 1
     dice1 = (random.randint(1, 6))
     dice2 = (random.randint(1, 6))
-    round = int(input("How many rounds do you want to play? "))
-    amount_of_money -= 1
+    if dice1 + dice2 != 7:
+        i -= 1
     if dice1 + dice2 == 7:
-        print("You win! You get $5 back.")
-        amount_of_money += 4
-        print(amount_of_money)
+        i += 4
+        print("You win! You get $4 back.")
+        print(i)
+    if i == 0:
+        print("It took this many %s to run out of money." % rounds)
