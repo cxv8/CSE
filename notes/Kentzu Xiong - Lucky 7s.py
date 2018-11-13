92b3a6f43d5d9d1fc3b8e59186df35fbcb13b843
cxv8 <44244680+cxv8@users.noreply.github.com>
2018-11-13 08:44:18 -0800

    most money

--- notes/Kentzu Xiong - Lucky 7s.py
+++ notes/Kentzu Xiong - Lucky 7s.py
@@ -1,17 +1,27 @@
 import random
-amount_of_money = 15
+i = 15
 rounds = 0
-print("You have $15 and bet $1. If you win you get $4 back.")
-i = amount_of_money
-while i in range(i > 0):
+print("You have $15 and bet $1. If you win you get $4.")
+most_money = i
+most_rounds = rounds
+while i > 0:
+    if most_money < i:
+        most_money = i
+    if most_rounds < rounds:
+        most_rounds = rounds
     rounds += 1
     dice1 = (random.randint(1, 6))
     dice2 = (random.randint(1, 6))
     if dice1 + dice2 != 7:
         i -= 1
+        print("you lose")
+        print(i)
     if dice1 + dice2 == 7:
         i += 4
-        print("You win! You get $4 back.")
+        print("you win")
         print(i)
     if i == 0:
-        print("It took this many %s to run out of money." % rounds)
+        print("The most money I had was %s" % most_money)
+        print("Round %s was the round I had the most money." % most_rounds)
+
+
