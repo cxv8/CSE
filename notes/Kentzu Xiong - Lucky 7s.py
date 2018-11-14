584202364e75f6ca8983ebe7dac7356b6e5aa836
cxv8 <44244680+cxv8@users.noreply.github.com>
2018-11-14 08:33:07 -0800

    complete Lucky 7s

--- notes/Kentzu Xiong - Lucky 7s.py
+++ notes/Kentzu Xiong - Lucky 7s.py
@@ -2,14 +2,15 @@ import random
 i = 15
 rounds = 0
 print("You have $15 and bet $1. If you win you get $4.")
+print("15")
 most_money = i
 most_rounds = rounds
+
 while i > 0:
+    rounds += 1
     if most_money < i:
         most_money = i
-    if most_rounds < rounds:
         most_rounds = rounds
-    rounds += 1
     dice1 = (random.randint(1, 6))
     dice2 = (random.randint(1, 6))
     if dice1 + dice2 != 7:
@@ -20,8 +21,8 @@ while i > 0:
         i += 4
         print("you win")
         print(i)
-    if i == 0:
-        print("The most money I had was %s" % most_money)
-        print("Round %s was the round I had the most money." % most_rounds)
+print("It took %d rounds to run out of money." % rounds)
+print("The most money I had was %d" % most_money)
+print("Round %d was the round I had the most money." % most_rounds)
 
 
