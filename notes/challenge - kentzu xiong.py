25e893031bec2296ba313e1647ddf859a8a2cf05
cxv8 <44244680+cxv8@users.noreply.github.com>
2018-12-04 08:43:22 -0800

    challnges

--- notes/challenge - kentzu xiong.py
+++ notes/challenge - kentzu xiong.py
@@ -5,8 +5,37 @@ def challenge7(number):
 challenge7(2)
 
 
-def challenge1(first,last):
+def challenge1(first, last):
     print(last, first)
 
 
 challenge1("Bob", "Dope")
+
+
+def challenge4(number):
+    if number >= 1:
+        print("positive")
+    if number <= -1:
+        print("negative")
+    if number == 0:
+        print("zero")
+
+
+challenge4(1)
+
+
+def challenge3(number, number1):
+    print(number*number1/2)
+
+
+challenge3(6,2)
+
+
+def challenge2(number):
+    if number == 1:
+        print("odd")
+    if number == 2:
+        print("even")
+
+
+challenge2(1)
