c4fa07107ebcdc70bfc3108b01704e08d6ec3a5b
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-01-08 08:44:29 -0800

    review

--- notes/Fix It - Kentzu Xiong.py
+++ notes/Fix It - Kentzu Xiong.py
@@ -1,8 +1,6 @@
-age= print(input("How old are you?"))
-print("How tall are you?", end=' ')
-height = input()
-print("How much do you weigh?", end=' ')
-weight = input()
+age= input("How old are you? ")
+height= input("How tall are you? ")
+weight= input("How much do you weigh? ")
 
 
 print("So, you're %s old, %s tall and %s heavy." % (age, height, weight))
@@ -42,7 +40,7 @@ beans, jars, crates = secret_formula(start_point)
 # remember that this is another way to format a string
 print("With a starting point of: %d" % start_point)
 # it's just like with an f"" string
-print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+print("We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
---
+++ semester 2 Notes - Kentzu Xiong.py
@@ -0,0 +1,16 @@
+print("Hello World")
+# Single-line comment
+car = 5
+driving = True
+
+print("I have %d cars" % car)
+print("I have " + str(car) + " car")
+
+age = input("How old are you? ")
+print("You are %d years old" % age)
+
+color = ['red', 'green', 'black', 'white', 'gold']
+color.append('pink')
+color.pop(0)
+
+print(color)
