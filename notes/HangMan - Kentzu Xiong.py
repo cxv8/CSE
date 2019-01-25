41ebed2f323ded7ad4bf0ca45b275de661e97c70
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-01-25 07:53:13 -0800

    hidden

--- notes/HangMan - Kentzu Xiong.py
+++ notes/HangMan - Kentzu Xiong.py
@@ -7,15 +7,17 @@ word_bank = ['turtle']
 word = random.choice(word_bank)
 guess_word = list(word)
 hidden = []
-hidden.append("*", len(range(guess_word))
-print(hidden)
+for i in guess_word:
+    hidden.append("*")
+print ("".join(hidden))
 guess = []
 while rounds >= 0 and guess != guess_word or guess != word:
-    guess.append(input("Guess a letter ".lower))
+    guess.append(input("Guess a letter "))
     for guess in guess_word:
         index = guess_word.index(guess)
         guess_word.pop(index)
         guess_word.insert(index, guess)
 
     print("".join(guess_word))
+
 
