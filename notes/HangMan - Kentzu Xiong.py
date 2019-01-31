d05f5138dee05f66311a44acd9ad14f5c3b461c6
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-01-30 22:59:31 -0800

    finished hangman

--- notes/HangMan - Kentzu Xiong.py
+++ notes/HangMan - Kentzu Xiong.py
@@ -1,23 +1,26 @@
 import random
 import string
 print("Guess the word")
-string.ascii_lowercase
+a = string.ascii_lowercase
+print("These are the letters " + a)
 rounds = 8
-word_bank = ['turtle']
+word_bank = ['turtle', 'pink', 'cat', 'dog', 'white', 'cyan', 'night', 'daytime', 'beach', 'lion']
 word = random.choice(word_bank)
 guess_word = list(word)
 hidden = []
-for i in guess_word:
+for i in range(len(guess_word)):
     hidden.append("*")
 print ("".join(hidden))
-guess = []
-while rounds >= 0 and guess != guess_word or guess != word:
-    guess.append(input("Guess a letter "))
-    for guess in guess_word:
-        index = guess_word.index(guess)
-        guess_word.pop(index)
-        guess_word.insert(index, guess)
+while rounds > 0 and hidden != guess_word:
+    guess = input("Guess a letter ").lower()
 
-    print("".join(guess_word))
+    if guess_word != guess:
+        rounds -= 1
+        for g in range(len(guess_word)):
+            if guess_word[g] == guess:
+                hidden[g] = guess
 
+    print("".join(hidden))
+    if hidden == guess_word:
+        print("you guessed it the word was " + word)
 
