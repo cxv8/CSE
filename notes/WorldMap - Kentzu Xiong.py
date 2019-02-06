2c3aa430dd3eda034850df1e7d1f0225f3c1a745
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-02-06 07:54:12 -0800

    worldd

--- notes/WorldMap - Kentzu Xiong.py
+++ notes/WorldMap - Kentzu Xiong.py
@@ -1,7 +1,27 @@
 world_map = {
     'House': {
         'NAME': "My House",
-        'DESCRIPTION': "You are at your house and getting out of bed"
-        'PATHS':
+        'DESCRIPTION': "You are in your house and getting out of bed."
+                       "There is a door east of your bed.",
+        'PATHS': {
+            'EAST': 'Hallway'
+        }
+    },
+    'Hallway': {
+        'NAME': "Hallway",
+        'DESCRIPTION': ' you are in the hallway and nobody in sight.'
+                       'Towards south is the living room.',
+        'PATHS': {
+            'SOUTH': 'Living room'
+        }
+    },
+    'Living room': {
+        "NAME": 'Living room',
+        'DESCRIPTION': 'You are in the Living room and nobody is there.'
+                       'To the west is the kitchen and and south is outside.',
+        'PATHS': {
+            'WEST': 'Kitchen',
+            'SOUTH': 'Outside'
+        }
     }
 }
