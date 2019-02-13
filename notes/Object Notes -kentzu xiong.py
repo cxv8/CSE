7cf13f00245aa028bbf0096b3cc48620e1701cfd
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-02-13 08:43:31 -0800

    object

---
+++ notes/Object Notes -kentzu xiong.py
@@ -0,0 +1,41 @@
+import Special_Random
+
+class WaterGun(object):
+    def __init__(self, capacity, distance=30, stock=False):
+        # These are things that a WaterGun has.
+        # All of these should be relevant to our program.
+        self.capacity = capacity
+        self.range = distance
+        self.trigger = True  # Someone's triggered?
+        self.stock = stock
+        self.duration_of_pressure = 5
+
+    def shoot(self, time):
+        if self.trigger:
+            if self.duration_of_pressure <= 0:
+                print("There's no pressure")
+            elif self.duration_of_pressure < time:
+                print("You run out of pressure after firing for %s seconds", self.duration_of_pressure)
+                self.duration_of_pressure = 0
+            else:
+                print("You shoot for %s seconds" % time)
+                self.duration_of_pressure -= time
+        else:
+            print("There's no trigger!")
+    def pump_it_up(self):
+        self.duration_of_pressure = 5
+        print("You pump the tank back to full pressure")
+
+# Initiate the objects
+my_water_gun = WaterGun(5.2, 40, True)
+your_water_gun = WaterGun(1.0, 1, False)
+wiebe_water_gun = WaterGun(9999999999, 9999999999999999999999999999, True)
+yahir_water_gun = WaterGun(0.1)
+# Do stuff w
+my_water_gun.shoot(5)
+my_water_gun.pump_it_up()
+my_water_gun.shoot(1)
+wiebe_water_gun.shoot(5)
+wiebe_water_gun.shoot(1)
+
+print(Special_Random.Randomwiebe.special_random())
---
+++ notes/Special_Random.py
@@ -0,0 +1,7 @@
+import random
+
+
+class Randomwiebe:
+    @staticmethod
+    def special_random():
+        return random.randint(1, 100)
