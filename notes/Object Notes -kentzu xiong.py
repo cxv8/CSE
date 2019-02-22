b799d921573611af9778f25a51c98a858b1a6e8c
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-02-22 08:43:15 -0800

    reee

--- notes/Object Notes -kentzu xiong.py
+++ notes/Object Notes -kentzu xiong.py
@@ -1,5 +1,6 @@
 import Special_Random
 
+
 class WaterGun(object):
     def __init__(self, capacity, distance=30, stock=False):
         # These are things that a WaterGun has.
@@ -22,15 +23,17 @@ class WaterGun(object):
                 self.duration_of_pressure -= time
         else:
             print("There's no trigger!")
+
     def pump_it_up(self):
         self.duration_of_pressure = 5
         print("You pump the tank back to full pressure")
+
 
 # Initiate the objects
 my_water_gun = WaterGun(5.2, 40, True)
 your_water_gun = WaterGun(1.0, 1, False)
 wiebe_water_gun = WaterGun(9999999999, 9999999999999999999999999999, True)
-yahir_water_gun = WaterGun(0.1)
+yahir_water_gun = WaterGun(0.1, 5)
 # Do stuff w
 my_water_gun.shoot(5)
 my_water_gun.pump_it_up()
--- notes/create object.py
+++ notes/create object.py
@@ -1,1 +1,31 @@
-class
+class paper(object):
+    def __init__(self,roll,airplane,boat,,box):
+        self.roll = roll
+        self.airplane = airplane
+        self.flower = flower
+        self.boat = boat
+        self.box = box
+
+    def roll(self):
+        if self.roll:
+
+        else: print("I rolled the paper")
+
+    def airplane(self):
+        if self.roll:
+            print("I turned the paper into an airplane")
+
+    def flower(self):
+        if self.draw:
+
+
+    def boat(self):
+        if self.write:
+            print("I am writing something")
+
+
+John = paper(1,1,1,1)
+
+John.draw()
+
+
