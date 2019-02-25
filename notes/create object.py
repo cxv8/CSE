8e67f0ed208a8b10196fe5dcdecaa568fa7de524
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-02-24 21:20:58 -0800

    ?

--- notes/create object.py
+++ notes/create object.py
@@ -1,31 +1,32 @@
-class paper(object):
-    def __init__(self,roll,airplane,boat,,box):
-        self.roll = roll
-        self.airplane = airplane
-        self.flower = flower
-        self.boat = boat
-        self.box = box
+class ToyCar(object):
+    def __init__(self,speed,motion=True,distance=30):
+        self.twist = 3
+        self.direction = True
+        self.speed = speed
+        self.motion = motion
+        self.distance = distance
 
-    def roll(self):
-        if self.roll:
+    def go(self, motion):
+        if self.motion:
+            if self.twist <= 0:
+                print("There is no twist.")
+            elif self.twist < motion:
+                print("After %s seconds the car stopped moving." % self.twist)
+                self.twist = 0
+            else:
+                print("The car went for %s seconds." % motion)
+                self.twist -= motion
+        else:
+            print("There is no twist.")
 
-        else: print("I rolled the paper")
-
-    def airplane(self):
-        if self.roll:
-            print("I turned the paper into an airplane")
-
-    def flower(self):
-        if self.draw:
-
-
-    def boat(self):
-        if self.write:
-            print("I am writing something")
-
-
-John = paper(1,1,1,1)
-
-John.draw()
+    def reload(self):
+        self.twist = 3
+        print("You twisted the car 3 times.")
 
 
+My_car = ToyCar(5,True,30)
+
+My_car.go(2)
+My_car.go(2)
+My_car.reload()
+My_car.go(3)
