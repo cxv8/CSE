9e2b8a7263f775653a784ab99224fb2bc267a82d
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-02-27 08:44:44 -0800

    n

--- notes/World Map OOP.py
+++ notes/World Map OOP.py
@@ -1,40 +1,39 @@
 class Room(object):
-    def __init__(self, name, north=None, south=None, east=None, description="", floor=None):
+    def __init__(self, name, north=None, south=None, east=None, description="",):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
         self.description = description
+        self.npc = []
+
+
 class Player(object):
-    def __init__(self,starting_location):
+    def __init__(self, starting_location):
         self.health = 100
         self.current_location = starting_location
-        self.chest_slot = 4
         self.inventory = []
         self.damage = 10
 
-def move(self, new_location):
-    """This method moves a character to a new location
+    def move(self, new_location):
+        """This method moves a character to a new location
+        :param new_location: The variable containing a room object
+        """
+        self.current_location = new_location
 
-    :param new_location: The variable containing a room object
-    """
-    self.current_location = new_location
 
-# Option 1
-# Add dependent rooms after
-R19A = Room("R19A")
-parking_lot = Room("The parking Lot", None, R19A)
-
-R19A.north = parking_lot
-
-# Option 2
 # Put them in quotes
 R19A = Room("R19A", 'parking_lot')
-parking_lot = Room("The parking Lot", None, 'R19A')
+parking_lot = Room('The parking Lot', None, "R19A")
 
+
+# Players
 player = Player(R19A)
+
+
 playing = True
-directions = ['north', 'south', 'west', 'east', 'up', ' down']
+directions = ['north', 'south', 'east', 'west', 'up', 'down']
+
 # Controller
 while playing:
     print(player.current_location.name)
@@ -42,17 +41,16 @@ while playing:
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
-    elif command.upper() in directions:
+    elif command.lower() in directions:
         try:
-            # command = north
-            room_object_that_we_move_to = getattr(player.current_location, command)
-
-            # needed for option 2
-
-            room_var = globals()[room_object]
+            # command = 'north'
+            room_name = getattr(player.current_location, command)
+            room_object = globals()[room_name]
 
             player.move(room_object)
         except KeyError:
+            print("I can't go that way.")
+        except AttributeError:
             print("I can't go that way.")
     else:
         print("Command Not Recognized")
