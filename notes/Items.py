8c5f2788811bf6f4ea26044036f355a907eef8f9
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-03-01 09:44:53 -0800

    ITEMS

---
+++ notes/Items.py
@@ -0,0 +1,8 @@
+class Item(object):
+    def __init__(self, name, material):
+        self.name = name
+        self.material = material
+
+
+class Weapon(Item):
+    def __init__(self, name, material, ):
