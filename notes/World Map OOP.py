b4b242697e0a77712ff1ba59bf479e454cab3fc8
cxv8 <44244680+cxv8@users.noreply.github.com>
2019-03-13 08:43:20 -0700

    damage

--- notes/World Map OOP.py
+++ notes/World Map OOP.py
@@ -22,14 +22,49 @@ class Player(object):
         self.current_location = new_location
 
 
+class Item(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Weapon(Item):
+    def __init__(self, damage, name):
+        super(Weapon, self).__init__(name)
+        self.damage = damage
+
+
+class Character(object):
+    def __init__(self, health, weapon, armor, name):
+        self.name = name
+        self.health = health
+        self.weapon = weapon
+        self.armor = armor
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+        print("%s has %d health left" % (self.name, self.health))
+            
+    def attack(self, target):
+        print("%s attacks %s for %d damage" % (self.name, target.name, self.weapon))
+        target.take_damage(self.weapon.damage)
+
 # Put them in quotes
 R19A = Room("R19A", 'parking_lot')
 parking_lot = Room('The parking Lot', None, "R19A")
 
+# Items
+sword = Weapon("Sword", 15)
+sword2 = Weapon("Orc Sword", 5)
 
 # Players
 player = Player(R19A)
 
+# Characters
+c1 = Character("Orc1", 100, sword, None)
+c2 = Character("Orc2", 100, sword2, None)
+c1.attack(c2)
 
 playing = True
 directions = ['north', 'south', 'east', 'west', 'up', 'down']
